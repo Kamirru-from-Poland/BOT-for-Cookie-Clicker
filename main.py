6fb5109f9af2e9a1e1d5da11e95cf30f8e7d564c
Kamirru <->
2022-08-31 10:53:15 +0200

    beta

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from selenium import webdriver
+import time
+def initPickALanguage():
+    element = window.find_element("xpath", "/html/body/div[2]/div[2]/div[12]/div/div[1]/div[1]/div[2]")
+    element.click()
+def clickACookie():
+    element = window.find_element("xpath", "/html/body/div[2]/div[2]/div[15]/div[8]/button")
+    element.click()
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("test123")
+    window = webdriver.Chrome("D:\chromedriver.exe")
+    window.get("https://orteil.dashnet.org/cookieclicker/")
+    time.sleep(2)
+    initPickALanguage()
+    time.sleep(2)
+    clickACookie()
+
+
