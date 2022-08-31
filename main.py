bbb079022221c4d36e998a11484a77b96a18e01a
Kamirru <->
2022-08-31 12:22:40 +0200

    done

--- main.py
+++ main.py
@@ -3,15 +3,28 @@ import time
 def initPickALanguage():
     element = window.find_element("xpath", "/html/body/div[2]/div[2]/div[12]/div/div[1]/div[1]/div[2]")
     element.click()
-def clickACookie():
-    element = window.find_element("xpath", "/html/body/div[2]/div[2]/div[15]/div[8]/button")
-    element.click()
+def buyItems():
+    buyMore = False
+    for item in items:
+        if item.get_attribute("class")=="product unlocked enabled":
+            item.click()
+            buyMore=True
+            break
+    if buyMore is True:
+        buyItems()
 if __name__ == '__main__':
     window = webdriver.Chrome("D:\chromedriver.exe")
     window.get("https://orteil.dashnet.org/cookieclicker/")
     time.sleep(2)
     initPickALanguage()
     time.sleep(2)
-    clickACookie()
-
-
+    cookie = window.find_element("xpath", "/html/body/div[2]/div[2]/div[15]/div[8]/button")
+    items = window.find_elements("xpath", "/html/body/div/div[2]/div[19]/div[3]/div[6]/*")
+    items.reverse()
+    i = 0
+    while True:
+        cookie.click()
+        i += 1
+        if i >= 100:
+            buyItems()
+            i=0
